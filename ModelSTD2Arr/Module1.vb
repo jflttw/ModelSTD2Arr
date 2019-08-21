26c7c8c63e78eb931121b8de8d041adb4d7a60ad
突突丸 <jflttw@outlook.com>
2019-08-21 14:33:54 +0800

    添加项目文件。

---
+++ ModelSTD2Arr/Module1.vb
@@ -0,0 +1,9 @@
+﻿Module Module1
+
+    Sub ModelSTD2Arr(BeamNo As Long, Section As String,
+    PointANo As Long, PointAx As Double, PointAy As Double, PointAz As Double,
+    PointBNo As Long, PointBx As Double, PointBy As Double, PointBz As Double)
+
+    End Sub
+
+End Module
